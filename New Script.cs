73216d33feedc45ee14550855eb79e5d4f0943cd
Yung_Hafner <89962794+YungHafner@users.noreply.github.com>
2021-10-24 18:20:54 +1000

    Add files via upload

---
+++ New Script.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+class Script
+{
+    [STAThread]
+    static public void Main(string[] args)
+    {
+        {
+			{
+				/*
+		      bool fw = false;
+			  Console.WriteLine("vvedi text");
+			  string words= Console.ReadLine();
+			  char [] Splitchars = new char [] {' ',' ','.'};
+			  string[] word =words.Split(Splitchars,StringSplitOptions.RemoveEmptyEntries);
+			  for(int i= 0; i<word.Length; i++)
+			  {
+				  if(word[i].Length==3)
+					  Console.WriteLine(word[i]);
+				  
+			  }
+
+			  Console.ReadLine();
+			  */
+			  
+			   int.TryParse(Console.ReadLine(), out int a);
+			   
+			   string o;
+			   o= Convert.ToString(Console.ReadLine());
+			   int.TryParse(Console.ReadLine(), out int b);
+			   int result;
+					if(o=="+")
+				{
+					result=a+b;
+					Console.WriteLine(result);
+				}
+					if(o=="-")
+				{
+					result=a-b;
+					Console.WriteLine(result);
+				} 
+					if(o=="*")
+				{
+					result=a*b;
+					Console.WriteLine(result);
+				}
+					if(o=="/")
+				{
+					result=a/b;
+					Console.WriteLine(result);
+				}
+				Console.ReadLine();
+			}		
+		}
+	}
+}
